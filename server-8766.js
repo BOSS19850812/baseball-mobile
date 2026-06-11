8f982799a95711f276897a169d1d580f3b336ea5
BOSS19850812 <esuroku.tv@gmail.com>
2026-06-11 17:38:31 +0900

    リファクタリング完了: クリーン版v25に置き換え

--- server-8766.js
+++ server-8766.js
@@ -14,4 +14,4 @@ const server = http.createServer((req,res)=>{
     res.end(data);
   });
 });
-server.listen(8766, '127.0.0.1', () => console.log('http://127.0.0.1:8766/baseball-mobile.html?v=v8'));
+server.listen(8766, '127.0.0.1', () => console.log('http://127.0.0.1:8766/baseball-mobile.html'));
--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,24 @@
-﻿const CACHE_NAME = "baseball-mobile-v24";
+const CACHE_NAME = "baseball-mobile-v25";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./baseball-mobile.html",
   "./manifest.json",
-  "./icon-new.png",
-  "./icon-180.png",
   "./icon-192.png",
   "./icon-512.png",
   "./apple-touch-icon.png"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
   self.clients.claim();
 });
-
 self.addEventListener("message", event => {
   if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
 });
-
 self.addEventListener("fetch", event => {
   const request = event.request;
   const wantsHtml = request.mode === "navigate" || (request.headers.get("accept") || "").includes("text/html");
@@ -38,9 +32,3 @@ self.addEventListener("fetch", event => {
   }
   event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
 });
-
-
-
-
-
-
