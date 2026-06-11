15bf065dcc79f88d4979954db4b63e2ecf139887
BOSS19850812 <esuroku.tv@gmail.com>
2026-06-11 14:19:56 +0900

    first commit

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+﻿const CACHE_NAME = "baseball-mobile-v23";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./baseball-mobile.html",
+  "./manifest.json",
+  "./icon-new.png",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  const request = event.request;
+  const wantsHtml = request.mode === "navigate" || (request.headers.get("accept") || "").includes("text/html");
+  if (wantsHtml) {
+    event.respondWith(fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      return response;
+    }).catch(() => caches.match(request)));
+    return;
+  }
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
+});
+
+
+
+
+
