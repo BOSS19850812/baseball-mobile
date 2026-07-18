51f6aaf63159fa74f527c059441967567a28a19d
BOSS19850812 <esuroku.tv@gmail.com>
2026-07-18 10:34:16 +0900

    fix live play order display

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "baseball-mobile-v84-steal-direct-pronunciation";
+﻿const CACHE_NAME = "baseball-mobile-v85-play-order-display-fix";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -32,6 +32,7 @@ self.addEventListener("fetch", event => {
   }
   event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
 });
+
 
 
 
