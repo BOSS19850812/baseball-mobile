7065f62a3350d925ee85784fb778c752bb91ba4e
BOSS19850812 <esuroku.tv@gmail.com>
2026-07-31 15:27:01 +0900

    v117: アカウントセクション簡素化

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "baseball-mobile-v116-catcher-front-pronunciation";
+﻿const CACHE_NAME = "baseball-mobile-v117-account-section-simplify";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -33,6 +33,7 @@ self.addEventListener("fetch", event => {
   }
   event.respondWith(caches.match(request).then(cached => cached || fetch(request)));
 });
+
 
 
 
